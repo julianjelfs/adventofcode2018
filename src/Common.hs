c701a8d3a7ee89d28bb43bf2b2c3fd9ccb45454e
Julian Jelfs <jjelfs@travelrepublic.co.uk>
2018-11-27 18:07:21 +0000

    add some dependencies

---
+++ src/Common.hs
@@ -0,0 +1,16 @@
+module Common where
+
+import           Control.Applicative           hiding ((<|>))
+import           Text.Parsec
+import           Text.ParserCombinators.Parsec (ParseError, Parser)
+
+numberParser :: Parsec String st Int
+numberParser = rd <$> (plus <|> minus <|> number)
+    where rd     = read :: String -> Int
+          plus   = char '+' *> number
+          minus  = (:) <$> char '-' <*> number
+          number = many1 digit
+
+parse :: Parser a -> String -> Either ParseError a
+parse parser = Text.Parsec.parse parser []
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
 module Main where
 
 main :: IO ()
-main = do
+main =
   putStrLn "hello world"
