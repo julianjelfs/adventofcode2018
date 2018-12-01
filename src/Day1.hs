658e753bcf921766e66b281884c15572d38c4c7c
Julian Jelfs <julian.jelfs@gmail.com>
2018-12-01 19:59:33 +0000

    make pattern mathc exhaustive

--- src/Day1.hs
+++ src/Day1.hs
@@ -8,7 +8,7 @@ import           Data.Maybe                     ( catMaybes )
 import qualified Data.Set                      as Set
 import           Text.Read                      ( readMaybe )
 
-solve :: IO Int
+solve :: IO (Maybe Int)
 solve = do
   inp <- cycle . catMaybes . fmap readNum . lines <$> readFile "data/day1.txt"
   return $ firstDup 0 Set.empty inp
@@ -17,8 +17,9 @@ solve = do
   readNum ('+' : xs) = readMaybe xs
   readNum str        = readMaybe str
 
-  firstDup :: Int -> Set.Set Int -> [Int] -> Int
+  firstDup :: Int -> Set.Set Int -> [Int] -> Maybe Int
+  firstDup freq prev [] = Nothing
   firstDup freq prev (x : xs)
-    | Set.member freq prev = freq
+    | Set.member freq prev = Just freq
     | otherwise            = firstDup (freq + x) (Set.insert freq prev) xs
 
