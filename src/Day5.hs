5e18c464932d733fe4b229f8ef67c18e232e07d7
Julian Jelfs <julian.jelfs@gmail.com>
2018-12-05 08:25:52 +0000

    day5 part two

--- src/Day5.hs
+++ src/Day5.hs
@@ -3,10 +3,10 @@
 module Day5 where
 
 import qualified Data.Char                     as Char
+import qualified Data.List                     as List
 import           Data.Text                      ( Text )
 import qualified Data.Text                     as Text
 import qualified Data.Text.IO                  as Text
-
 
 data Unit = Unit Text deriving Show
 
@@ -30,10 +30,13 @@ partOne = do
   let (Unit solved) = solve inp
   pure $ (Text.length solved) - 1
 
-partTwo :: IO [(Char, Int)]
+partTwo :: IO (Char, Int)
 partTwo = do
   inp <- Text.readFile "data/day5.txt"
-  pure $ findShortest inp
+  pure . List.minimumBy shortest $ mapLengths <$> findShortest inp
+ where
+  mapLengths (c, Unit u) = (c, Text.length u)
+  shortest (_, l) (_, l') = l `compare` l'
 
 solve :: Text -> Unit
 solve t = foldMap id allunits
@@ -41,15 +44,14 @@ solve t = foldMap id allunits
   allunits :: [Unit]
   allunits = Text.foldr (\c units -> Unit (Text.singleton c) : units) [] t
 
-findShortest :: Text -> [(Char, Int)]
+findShortest :: Text -> [(Char, Unit)]
 findShortest t = solveWithout t <$> ['a' .. 'z']
 
-solveWithout :: Text -> Char -> (Char, Int)
-solveWithout t c =
-  let (Unit solved) = solve $ Text.filter (matchesChar c) t
-  in  (c, Text.length solved)
+solveWithout :: Text -> Char -> (Char, Unit)
+solveWithout t c = (c, solve $ Text.filter (notChar c) t)
 
-matchesChar :: Char -> Char -> Bool
-matchesChar l c | l == c                        = True
-                | Char.ord l == Char.ord c - 32 = True
-                | otherwise                     = False
+notChar :: Char -> Char -> Bool
+notChar l c = Char.ord l /= Char.ord c + 32 && l /= c
+
+testInput :: Text
+testInput = "dabAcCaCBAcCcaDA"
