269e289504e78b1f80717f137bea7c58916afd22
Julian Jelfs <jjelfs@travelrepublic.co.uk>
2018-12-05 09:14:45 +0000

    simplification

--- src/Day5.hs
+++ src/Day5.hs
@@ -2,21 +2,20 @@
 
 module Day5 where
 
-import qualified Data.Char                     as Char
-import qualified Data.List                     as List
-import           Data.Text                      ( Text )
-import qualified Data.Text                     as Text
-import qualified Data.Text.IO                  as Text
+import qualified Data.Char    as Char
+import           Data.Text    (Text)
+import qualified Data.Text    as Text
+import qualified Data.Text.IO as Text
 
-data Unit = Unit Text deriving Show
+newtype Unit = Unit Text deriving Show
 
 instance Semigroup Unit where
     (Unit "") <> (Unit b) = Unit b
     (Unit a) <> (Unit "") = Unit a
     (Unit a) <> (Unit b) =
-        case oppositeCase (Text.last a) (Text.head b) of
-            True  -> (Unit $ Text.dropEnd 1 a) <> (Unit $ Text.drop 1 b)
-            False -> Unit $ a <> b
+        if oppositeCase (Text.last a) (Text.head b)
+        then (Unit $ Text.dropEnd 1 a) <> (Unit $ Text.drop 1 b)
+        else Unit $ a <> b
 
 instance Monoid Unit where
     mempty = Unit mempty
@@ -28,15 +27,14 @@ partOne :: IO Int
 partOne = do
   inp <- Text.readFile "data/day5.txt"
   let (Unit solved) = solve inp
-  pure $ (Text.length solved) - 1
+  pure $ Text.length solved - 1
 
-partTwo :: IO (Char, Int)
+partTwo :: IO Int
 partTwo = do
   inp <- Text.readFile "data/day5.txt"
-  pure . List.minimumBy shortest $ mapLengths <$> findShortest inp
+  pure . minimum $ mapLengths <$> findShortest inp
  where
-  mapLengths (c, Unit u) = (c, Text.length u)
-  shortest (_, l) (_, l') = l `compare` l'
+  mapLengths (Unit u) = Text.length u
 
 solve :: Text -> Unit
 solve t = foldMap id allunits
@@ -44,11 +42,11 @@ solve t = foldMap id allunits
   allunits :: [Unit]
   allunits = Text.foldr (\c units -> Unit (Text.singleton c) : units) [] t
 
-findShortest :: Text -> [(Char, Unit)]
+findShortest :: Text -> [Unit]
 findShortest t = solveWithout t <$> ['a' .. 'z']
  where
-  solveWithout :: Text -> Char -> (Char, Unit)
-  solveWithout t c = (c, solve $ Text.filter (notChar c) t)
+  solveWithout :: Text -> Char -> Unit
+  solveWithout t c = solve $ Text.filter (notChar c) t
 
   notChar :: Char -> Char -> Bool
   notChar l c = Char.ord l /= Char.ord c + 32 && l /= c
