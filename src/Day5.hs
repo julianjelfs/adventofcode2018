06971fba6356ef3f57f14e3bcf89cbf9285d9784
Julian Jelfs <jjelfs@travelrepublic.co.uk>
2018-12-05 09:27:36 +0000

    golfing

--- src/Day5.hs
+++ src/Day5.hs
@@ -23,21 +23,16 @@ instance Monoid Unit where
 oppositeCase :: Char -> Char -> Bool
 oppositeCase c c' = abs (Char.ord c' - Char.ord c) == 32
 
-partOne :: IO Int
-partOne = do
-  inp <- Text.readFile "data/day5.txt"
-  let (Unit solved) = solve inp
-  pure $ Text.length solved - 1
-
-partTwo :: IO Int
-partTwo = do
-  inp <- Text.readFile "data/day5.txt"
-  pure . minimum $ mapLengths <$> findShortest inp
+solution :: IO (Int, Int)
+solution = do
+  inp <- Text.dropEnd 1 <$> Text.readFile "data/day5.txt"
+  let (Unit solved) = reactPolymer inp
+  pure (Text.length solved, minimum $ mapLengths <$> findShortest inp)
  where
   mapLengths (Unit u) = Text.length u
 
-solve :: Text -> Unit
-solve t = foldMap id allunits
+reactPolymer :: Text -> Unit
+reactPolymer t = foldMap id allunits
  where
   allunits :: [Unit]
   allunits = Text.foldr (\c units -> Unit (Text.singleton c) : units) [] t
@@ -46,7 +41,7 @@ findShortest :: Text -> [Unit]
 findShortest t = solveWithout t <$> ['a' .. 'z']
  where
   solveWithout :: Text -> Char -> Unit
-  solveWithout t c = solve $ Text.filter (notChar c) t
+  solveWithout t c = reactPolymer $ Text.filter (notChar c) t
 
   notChar :: Char -> Char -> Bool
   notChar l c = Char.ord l /= Char.ord c + 32 && l /= c
