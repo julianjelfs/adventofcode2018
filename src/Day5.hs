0a4bbea4391f0b6c5f068f7f367b09cc25aa6cb4
Julian Jelfs <jjelfs@travelrepublic.co.uk>
2018-12-05 10:43:12 +0000

    obsessive tidy

--- src/Day5.hs
+++ src/Day5.hs
@@ -8,12 +8,12 @@ import           Data.Text     (Text)
 import qualified Data.Text     as Text
 import qualified Data.Text.IO  as Text
 
-newtype Unit = Unit Text deriving Show
+newtype Unit = Unit { unUnit :: Text } deriving Show
 
 instance Semigroup Unit where
-    (Unit "") <> (Unit b) = Unit b
-    (Unit a) <> (Unit "") = Unit a
-    (Unit a) <> (Unit b) =
+    (Unit "") <> (Unit b)  = Unit b
+    (Unit a)  <> (Unit "") = Unit a
+    (Unit a)  <> (Unit b)  =
         if oppositeCase (Text.last a) (Text.head b)
         then (Unit $ Text.dropEnd 1 a) <> (Unit $ Text.drop 1 b)
         else Unit $ a <> b
@@ -27,10 +27,10 @@ oppositeCase c c' = abs (Char.ord c' - Char.ord c) == 32
 solution :: IO (Int, Int)
 solution = do
   inp <- Text.dropEnd 1 <$> Text.readFile "data/day5.txt"
-  let (Unit solved) = reactPolymer inp
-  pure (Text.length solved, minimum $ mapLengths <$> findShortest inp)
+  pure (partOne inp, partTwo inp)
  where
-  mapLengths (Unit u) = Text.length u
+  partOne = Text.length . unUnit . reactPolymer
+  partTwo = findShortest
 
 reactPolymer :: Text -> Unit
 reactPolymer t = fold allunits
@@ -38,8 +38,8 @@ reactPolymer t = fold allunits
   allunits :: [Unit]
   allunits = Text.foldr (\c units -> Unit (Text.singleton c) : units) [] t
 
-findShortest :: Text -> [Unit]
-findShortest t = solveWithout t <$> ['a' .. 'z']
+findShortest :: Text -> Int
+findShortest t = minimum $ Text.length . unUnit . solveWithout t <$> ['a' .. 'z']
  where
   solveWithout :: Text -> Char -> Unit
   solveWithout t c = reactPolymer $ Text.filter (notChar c) t
