8083d0ff2435c1436baa99b192d6649136952a8f
Julian Jelfs <jjelfs@travelrepublic.co.uk>
2018-12-06 09:30:55 +0000

    day6 part one

--- src/Day6.hs
+++ src/Day6.hs
@@ -4,34 +4,68 @@ import qualified Common                        as C
 import qualified Data.Map                      as M
 import qualified Data.Set                      as S
 import qualified Text.Parsec                   as P
-import           Text.ParserCombinators.Parsec  ( Parser )
+import           Text.ParserCombinators.Parsec (Parser)
 
 type Point = (Int, Int)
 type GridPoint = (Int, Int)
 type Areas = M.Map Point (S.Set GridPoint)
 
-partOne :: IO Areas
+testPoints :: [Point]
+testPoints =
+    [ (1,1)
+    , (1,6)
+    , (8,3)
+    , (3,4)
+    , (5,5)
+    , (8,9)
+    ]
+
+partOne :: IO Int
 partOne = do
   inp <- traverse (C.parse coordParser) . lines <$> readFile "data/day6.txt"
   pure $ case inp of
-    Left  _ -> M.empty
-    Right c -> foldr (nearestPoint c) M.empty (grid c)
-        --nearestPoint M.empty c <$> grid c
-
-    --so far we have a map of all points in the grid to the coord they are
-    --closest to
+    Left  _ -> 0
+    Right c -> largestArea $ removeInfinites c $ foldr (nearestPoint c) M.empty (grid c)
 
 grid :: [Point] -> [GridPoint]
 grid points = [ (x, y) | x <- [minx .. maxx], y <- [miny .. maxy] ]
   where ((minx, miny), (maxx, maxy)) = getBounds points
 
+largestArea :: Areas -> Int
+largestArea =
+    M.foldr
+        (\s max ->
+            if S.size s > max
+            then S.size s
+            else max) 0
+
+removeInfinites :: [Point] -> Areas -> Areas
+removeInfinites points =
+    M.filterWithKey
+        (\k _ -> not $ infiniteArea points k)
+
 nearestPoint :: [Point] -> GridPoint -> Areas -> Areas
-nearestPoint points gridPoint areas = undefined
-    --manhattanDistance gridPoint <$> points
+nearestPoint points gridPoint areas =
+    let res = foldr
+            (\p agg ->
+                let m = manhattanDistance gridPoint p
+                in case agg of
+                    Nothing -> Just ([p], m)
+                    Just (ps, m')
+                        | m < m' -> Just ([p], m)
+                        | m == m' -> Just (p:ps, m)
+                        | otherwise -> agg
+            ) Nothing points
+    in case res of
+        Just ([p], _) ->
+            if p `M.member` areas
+            then M.update (Just . S.insert gridPoint) p areas
+            else M.insert p (S.singleton gridPoint) areas
+        _             -> areas
 
 --a point's are will be infinite if it is not surrounded by other points
-isInfinite :: [Point] -> Point -> Bool
-isInfinite points point =
+infiniteArea :: [Point] -> Point -> Bool
+infiniteArea points point =
   not
     $  any (left point)  points
     && any (right point) points
@@ -51,7 +85,7 @@ isInfinite points point =
   down (_, y1) (_, y2) = y2 > y1
 
 manhattanDistance :: GridPoint -> Point -> Int
-manhattanDistance (x1, y1) (x2, y2) = (x1 - x2) + (y1 - y2)
+manhattanDistance (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
 
 getBounds :: [Point] -> (Point, Point)
 getBounds points =
