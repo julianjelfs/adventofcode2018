f2ef588a3bb0a7f61176c94fcd52c9102e893f21
Julian Jelfs <jjelfs@travelrepublic.co.uk>
2018-12-07 12:21:08 +0000

    re-jig

--- src/Day7.hs
+++ src/Day7.hs
@@ -2,35 +2,41 @@ module Day7 where
 
 import qualified Common                        as C
 import qualified Data.List                     as L
+import qualified Data.Map                      as M
 import qualified Data.Set                      as S
 import qualified Text.Parsec                   as P
-import           Text.ParserCombinators.Parsec  ( Parser )
+import           Text.ParserCombinators.Parsec (Parser)
+
+type Dependencies = M.Map Char (S.Set Char)
 
 solution :: IO String
 solution = do
   inp <- traverse (C.parse instrParser) . lines <$> readFile "data/day7.txt"
   pure $ case inp of
     Left  _ -> undefined
-    Right i -> partOne i
+    Right i -> partOne $ parse i
 
-partOne :: [(Char, Char)] -> String
-partOne instr = go [] instr
- where
-  go seq i = case findAvailableStep (allSteps i) i of
-    Nothing -> seq <> S.toList (S.difference (allSteps instr) (S.fromList seq))
-    Just c  -> go (seq <> [c]) (removeCompleteStep i c)
+partOne :: Dependencies -> String
+partOne = go
+  where
+    go d =
+      case nextStep d of
+        Just (c, _) -> c : go (M.map (S.delete c) $ M.delete c d)
+        Nothing     -> []
 
+    nextStep :: Dependencies -> Maybe (Char, S.Set Char)
+    nextStep = M.lookupMin . M.filter S.null
+
+parse :: [(Char, Char)] -> Dependencies
+parse i =
+    foldr (\c m -> M.insert c (dependentSteps c i) m) M.empty (allSteps i)
+
+dependentSteps :: Char -> [(Char, Char)] -> S.Set Char
+dependentSteps c i =
+    S.fromList . L.sort $ fst <$> filter (\(_, a) -> a == c) i
 
 allSteps :: [(Char, Char)] -> S.Set Char
 allSteps = foldr (\(a, b) s -> S.insert a (S.insert b s)) S.empty
-
-removeCompleteStep :: [(Char, Char)] -> Char -> [(Char, Char)]
-removeCompleteStep instrs c = filter (\(a, _) -> a /= c) instrs
-
-findAvailableStep :: S.Set Char -> [(Char, Char)] -> Maybe Char
-findAvailableStep all instrs = C.safeHead . L.sort . S.toList $ S.difference
-  all
-  (S.fromList $ snd <$> instrs)
 
 instrParser :: Parser (Char, Char)
 instrParser = do
@@ -38,15 +44,15 @@ instrParser = do
   after  <- P.string " must be finished before step " *> P.anyChar
   pure (before, after)
 
--- testInput :: [(Char, Char)]
--- testInput =
---   [ ('C', 'A')
---   , ('C', 'F')
---   , ('A', 'B')
---   , ('A', 'D')
---   , ('B', 'E')
---   , ('D', 'E')
---   , ('F', 'E')
---   ]
+testInput :: [(Char, Char)]
+testInput =
+  [ ('C', 'A')
+  , ('C', 'F')
+  , ('A', 'B')
+  , ('A', 'D')
+  , ('B', 'E')
+  , ('D', 'E')
+  , ('F', 'E')
+  ]
 
 
