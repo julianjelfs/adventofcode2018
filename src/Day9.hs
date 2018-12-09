76df2d459f675e7bef7fb5eb11058f93a9ac88f3
Julian Jelfs <julian.jelfs@gmail.com>
2018-12-09 17:08:57 +0000

    get fucked day9

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,32 +1,37 @@
 module Day9 where
 
+import qualified Data.List.PointedList.Circular
+                                               as P
+import           Data.Maybe                     ( fromJust )
 import           Data.Sequence                  ( Seq
                                                 , (!?)
                                                 )
 import qualified Data.Sequence                 as Seq
---import           Debug.Trace                    ( traceShowId )
+import           Debug.Trace                    ( traceShowId )
 
-solution :: ((Seq Int, Seq Int), Int)
+solution :: ((P.PointedList Int, Seq Int), Int)
 solution = (partOne, partTwo)
 
-partOne :: (Seq Int, Seq Int)
-partOne = go (Seq.replicate numWorkers 0) (Seq.fromList [0]) 0 1
+partOne :: (P.PointedList Int, Seq Int)
+partOne = go (Seq.replicate numWorkers 0) (P.singleton 0) 1
  where
-  go :: Seq Int -> Seq Int -> Int -> Int -> (Seq Int, Seq Int)
-  go workers circle ix n
+  go :: Seq Int -> P.PointedList Int -> Int -> (P.PointedList Int, Seq Int)
+  go workers circle n
     | n `mod` 23 == 0
-    = let (v, ix', circle') = sevenBack circle ix
-          wi                = workerIndex n
-          currentScore      = workers !? wi
-          workers'          = case currentScore of
+    = let circle'      = P.moveN (-7) circle
+          f            = P._focus circle'
+          wi           = workerIndex n
+          currentScore = workers !? wi
+          workers'     = case currentScore of
             Nothing -> workers
-            Just s  -> Seq.update wi (s + n + v) workers
-      in  if n + v == lastMarble
-            then (circle', workers')
-            else go workers' circle' ix' (n + 1)
+            Just s  -> Seq.update wi (s + n + f) workers
+          circle'' = fromJust $ P.deleteRight circle'
+      in  if n + f == lastMarble
+            then (circle'', workers')
+            else go workers' circle'' (n + 1)
     | otherwise
-    = let ix' = nextIndex circle ix
-      in  go workers (Seq.insertAt ix' n circle) ix' (n + 1)
+    = let circle' = P.insertLeft n . P.moveN 2 $ circle
+      in  go workers (traceShowId circle') (n + 1)
 
 workerIndex :: Int -> Int
 workerIndex marble = (marble - 1) `mod` numWorkers
@@ -39,17 +44,3 @@ numWorkers = 9
 
 lastMarble :: Int
 lastMarble = 32
-
-sevenBack :: Seq Int -> Int -> (Int, Int, Seq Int)
-sevenBack s i =
-  let i'  = i - 7
-      i'' = if i' < 0 then Seq.length s + i' else i'
-      v   = s !? i''
-  in  case v of
-        Nothing -> (0, i, s)
-        Just v' -> (v', i'', Seq.deleteAt i'' s)
-
-nextIndex :: Seq Int -> Int -> Int
-nextIndex s i = case (i + 2) `mod` Seq.length s of
-  0 -> Seq.length s
-  n -> n
