1756c3f44cee455b670726126169cd0966613a85
Julian Jelfs <jjelfs@travelrepublic.co.uk>
2018-12-13 12:20:11 +0000

    day13 part 2 wip

--- src/Day13.hs
+++ src/Day13.hs
@@ -2,15 +2,25 @@
 
 module Day13 where
 
-import qualified Data.List                     as L
-import           Data.Map                       ( Map )
-import qualified Data.Map                      as M
+import qualified Data.List as L
+import           Data.Map  (Map)
+import qualified Data.Map  as M
+import           Data.Set  (Set)
+import qualified Data.Set  as S
 
 type Coord = (Int,Int)
 
-data Car = Car Coord Direction deriving Show
+data Car = Car Coord NextTurn Direction deriving Show
+
+instance Eq Car where
+    (Car c _ _) == (Car c' _ _) = c == c'
+
+instance Ord Car where
+    (Car c _ _) `compare` (Car c' _ _) = c `compare` c'
 
 data Direction = U | D | L | R deriving Show
+
+data NextTurn = TurnLeft | TurnRight | GoStraight deriving Show
 
 data Track
     = Vertical
@@ -21,30 +31,83 @@ data Track
     deriving Show
 
 data State = State
-    { cars  :: [Car]
+    { cars  :: Set Car
     , track :: Map Coord Track
     } deriving Show
 
-solution :: IO Coord
-solution = do
-  state <- parseState . lines <$> readFile "data/day13.txt"
-  pure $ eventLoop state
+solution :: IO (Maybe Coord)
+solution =
+  eventLoop . parseState . lines <$> readFile "data/day13.txt"
 
-eventLoop :: State -> Coord
+eventLoop :: State -> Maybe Coord
 eventLoop State {..} =
-  let cars' = moveCar track <$> cars
-  in  case collision cars' of
-        Nothing    -> eventLoop (State {cars = cars', ..})
-        Just coord -> coord
- where
-  moveCar :: Map Coord Track -> Car -> Car
-  moveCar _t c = c
+    let updatedCars =
+            L.foldl'
+                (\cars' car ->
+                      let car'@(Car c' _ _) = moveCar track car
+                      in case M.lookup c' cars' of
+                        Nothing -> M.insert c' [car'] cars'
+                        Just _  -> M.update (\l -> Just $ car' : l) c' cars'
+                ) M.empty (L.sort $ S.toList cars)
+        deduped = M.filter (\l -> L.length l == 1) updatedCars
+    in case M.size deduped of
+            1 -> lastCarCoord deduped
+            _ -> eventLoop (State { cars = S.fromList $ M.foldr' (<>) [] deduped, ..})
 
-  collision :: [Car] -> Maybe Coord
-  collision _cars = undefined
+lastCarCoord :: Map Coord [Car] -> Maybe Coord
+lastCarCoord m =
+    case M.toList m of
+        [(c, _)] -> Just c
+        _        -> Nothing
+
+moveCar :: Map Coord Track -> Car -> Car
+moveCar t (Car (x,y) n U) =
+    case M.lookup (x, y-1) t of
+        Just Vertical -> Car (x, y-1) n U
+        Just Backslash -> Car (x, y-1) n L
+        Just Forwardslash -> Car (x, y-1) n R
+        Just Intersection -> Car (x, y-1) (nextTurn n) (makeTurn n U)
+        _ -> error "the track and the car are in a fucked up state"
+moveCar t (Car (x,y) n D) =
+    case M.lookup (x, y+1) t of
+        Just Vertical -> Car (x, y+1) n D
+        Just Backslash -> Car (x, y+1) n R
+        Just Forwardslash -> Car (x, y+1) n L
+        Just Intersection -> Car (x, y+1) (nextTurn n) (makeTurn n D)
+        _ -> error "the track and the car are in a fucked up state"
+moveCar t (Car (x,y) n L) =
+    case M.lookup (x-1, y) t of
+        Just Horizontal -> Car (x-1, y) n L
+        Just Backslash -> Car (x-1, y) n U
+        Just Forwardslash -> Car (x-1, y) n D
+        Just Intersection -> Car (x-1, y) (nextTurn n) (makeTurn n L)
+        _ -> error "the track and the car are in a fucked up state"
+moveCar t (Car (x,y) n R) =
+    case M.lookup (x+1, y) t of
+        Just Horizontal -> Car (x+1, y) n R
+        Just Backslash -> Car (x+1, y) n D
+        Just Forwardslash -> Car (x+1, y) n U
+        Just Intersection -> Car (x+1, y) (nextTurn n) (makeTurn n R)
+        _ -> error "the track and the car are in a fucked up state"
+
+makeTurn :: NextTurn -> Direction -> Direction
+makeTurn TurnLeft U   = L
+makeTurn TurnLeft D   = R
+makeTurn TurnLeft L   = D
+makeTurn TurnLeft R   = U
+makeTurn TurnRight U  = R
+makeTurn TurnRight D  = L
+makeTurn TurnRight L  = U
+makeTurn TurnRight R  = D
+makeTurn GoStraight d = d
+
+nextTurn :: NextTurn -> NextTurn
+nextTurn TurnLeft   = GoStraight
+nextTurn GoStraight = TurnRight
+nextTurn TurnRight  = TurnLeft
 
 parseState :: [String] -> State
-parseState lines = L.foldl' rows (State [] M.empty) (zip [0 ..] lines)
+parseState lines = L.foldl' rows (State S.empty M.empty) (zip [0 ..] lines)
  where
   rows :: State -> (Int, String) -> State
   rows s (y, row) = L.foldl' (cols y) s (zip [0 ..] row)
@@ -57,7 +120,7 @@ parseState lines = L.foldl' rows (State [] M.empty) (zip [0 ..] lines)
     '/'  -> addTrack (x, y) Forwardslash s
     '>'  -> addTrack (x, y) Horizontal (addCar (x, y) R s)
     '<'  -> addTrack (x, y) Horizontal (addCar (x, y) L s)
-    'V'  -> addTrack (x, y) Vertical (addCar (x, y) D s)
+    'v'  -> addTrack (x, y) Vertical (addCar (x, y) D s)
     '^'  -> addTrack (x, y) Vertical (addCar (x, y) U s)
     '+'  -> addTrack (x, y) Intersection s
     _    -> s
@@ -66,5 +129,5 @@ parseState lines = L.foldl' rows (State [] M.empty) (zip [0 ..] lines)
   addTrack c t s = s { track = M.insert c t (track s) }
 
   addCar :: Coord -> Direction -> State -> State
-  addCar c d s = s { cars = Car c d : cars s }
+  addCar c d s = s { cars = S.insert (Car c TurnLeft d) (cars s) }
 
