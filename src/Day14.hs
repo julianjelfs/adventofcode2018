2c82f4eb8548e95c209f22c3291e4125ce532549
Julian Jelfs <julian.jelfs@gmail.com>
2018-12-21 20:52:35 +0000

    terrible bollocks

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,40 +1,48 @@
+{-# LANGUAGE ViewPatterns #-}
 module Day14 where
 
-import qualified Data.List.Extra as L
---import           Debug.Trace     (traceShowId)
+import           Data.Foldable                  ( toList )
+import           Data.Sequence                  ( Seq
+                                                , (><)
+                                                )
+import qualified Data.Sequence                 as S
 
--- solution :: Int -> (Seq Int, Int)
--- solution n =
---   (partOne, 0)
---   where
---     _input = [5,1,5,8,9]
+import qualified Data.List.Extra               as L
 
---     partOne = S.take 10 . S.drop n $ makeRecipesUntil (\s -> S.length s >= (n + 10)) initial
---
---  probably we want to build this in reverse order
-
-initial :: (Int, Int, [Int])
-initial = (0,1,[3,7])
+initial :: (Int, Int, Seq Int)
+initial = (0, 1, S.fromList [3, 7])
 
 partTwo :: [Int] -> Int
-partTwo input = length . drop (length input) $ makeRecipesUntil input initial
+partTwo input = length . (L.dropEnd 7) . toList $ makeRecipe input [] initial
 
-makeRecipesUntil :: [Int] -> (Int, Int, [Int]) -> [Int]
-makeRecipesUntil input (e1, e2, recipes) =
-  let e1Recipe  = recipes !! e1
-      e2Recipe  = recipes !! e2
+makeRecipe :: [Int] -> [Int] -> (Int, Int, Seq Int) -> Seq Int
+makeRecipe input recent (e1, e2, recipes) =
+  let e1Recipe  = S.index recipes e1
+      e2Recipe  = S.index recipes e2
       newRecipe = splitDigits $ e1Recipe + e2Recipe
-      recipes'  = recipes <> newRecipe
-      l         = length recipes'
+      recipes'  = recipes >< newRecipe
+      l         = S.length recipes'
       nextParam = (newIndex e1 e1Recipe l, newIndex e2 e2Recipe l, recipes')
+  in  if finished input recent
+        then recipes
+        else makeRecipe input (moveWindow input recent newRecipe) nextParam
 
-  in if L.takeEnd (length input) recipes'  == input
-     then recipes'
-     else makeRecipesUntil input nextParam
+moveWindow :: [Int] -> [Int] -> Seq Int -> [Int]
+moveWindow (length -> n) recent recipes =
+  let window = recent <> toList recipes
+      l      = length window
+  in  if l > (n + 1) then drop (l - (n + 1)) window else window
+
+
+finished :: [Int] -> [Int] -> Bool
+finished input recent | input == recent                    = True
+                      | input `L.isPrefixOf` recent        = True
+                      | input `L.isPrefixOf` drop 1 recent = True
+                      | otherwise                          = False
 
 newIndex :: Int -> Int -> Int -> Int
 newIndex c i l = (c + i + 1) `mod` l
 
-splitDigits :: Int -> [Int]
-splitDigits i | i < 10    = [i]
-              | otherwise = [i `div` 10, i `rem` 10]
+splitDigits :: Int -> Seq Int
+splitDigits i | i < 10    = S.singleton i
+              | otherwise = S.fromList [i `div` 10, i `rem` 10]
