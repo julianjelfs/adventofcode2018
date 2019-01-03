c63c1bfb26ea8f50c5afccc51c7efa46592034fe
Julian Jelfs <julian.jelfs@gmail.com>
2019-01-03 08:00:48 +0000

    day 16 part two wip

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Day16 where
 
 import qualified Common                        as C
@@ -8,15 +10,19 @@ import qualified Data.Array                    as A
 import           Data.Bits                      ( (.&.)
                                                 , (.|.)
                                                 )
+import qualified Data.List                     as L
+import qualified Data.Map                      as M
 import qualified Text.Parsec                   as P
 import           Text.ParserCombinators.Parsec  ( Parser )
 
 type Register = A.Array Int Int
 
+type Processor = Register -> Instr -> Register
+
 register :: A.Array Int Int
 register = A.listArray (0, 3) [0, 0, 0, 0]
 
-allOperations :: [Register -> Instr -> Register]
+allOperations :: [Processor]
 allOperations =
   [ addr
   , addi
@@ -36,10 +42,12 @@ allOperations =
   , eqrr
   ]
 
+opsMap :: M.Map Int Processor
+opsMap = M.fromList $ zip [0 ..] allOperations
 
 
 addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr
-  :: Register -> Instr -> Register
+  :: Processor
 addr = binopr (+)
 addi = binopi (+)
 mulr = binopr (*)
@@ -73,9 +81,6 @@ data Instr = Instr Int Int Int Int deriving Show
 
 data SampleInstr = SampleInstr Register Instr Register deriving Show
 
-solve :: IO Int
-solve = undefined
-
 partOne :: IO Int
 partOne = do
   samples <- parseSamples
@@ -89,9 +94,46 @@ partOne = do
             )
         <$> samples'
 
+partTwo :: IO [Int]
+partTwo = do
+  samples <- parseSamples
+  instrs  <- parseInstrs
+  pure $ case (samples, instrs) of
+    (Left err      , _             ) -> error $ show err
+    (_             , Left err      ) -> error $ show err
+    (Right samples', Right _instrs') -> M.keys $ decode M.empty samples' opsMap
 
-parseRaw :: IO (Either P.ParseError [Instr])
-parseRaw =
+
+decode
+  :: M.Map Int Processor
+  -> [SampleInstr]
+  -> M.Map Int Processor
+  -> M.Map Int Processor
+decode decoded _ (M.null -> True) = decoded
+decode decoded samples encoded =
+  let (d, e) = L.foldr
+        (\(SampleInstr r1 i@(Instr opcode _ _ _) r2) (decoded', encoded') ->
+          case
+              filter fourth
+              $   (\(k, op) -> (opcode, op, k, r2 == op r1 i))
+              <$> M.toList encoded'
+            of
+              [(code, op, k, _)] ->
+                ( M.insert code op decoded'
+                , M.filterWithKey (\k' _ -> k /= k') encoded'
+                )
+              _ -> (decoded', encoded')
+        )
+        (decoded, encoded)
+        samples
+  in  decode d samples e
+
+fourth :: (Int, Processor, Int, Bool) -> Bool
+fourth (_, _, _, a) = a
+
+
+parseInstrs :: IO (Either P.ParseError [Instr])
+parseInstrs =
   traverse (C.parse parseInstr) . lines <$> readFile "data/day16_2.txt"
 
 parseSamples :: IO (Either P.ParseError [SampleInstr])
